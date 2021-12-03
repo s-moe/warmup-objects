9eb9dda9d1a835fc497ae9729f6b18bc087a39ba
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-12-03 19:06:56 +0000

    numbers as keys converted to string

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const person = {firstName: "Mick", lastName: "Jagger"}
 
 console.log(person.firstName)
 console.log(person["firstName"])
+
+//all keys are converted to strings
+const years = {1999: 'Good', 2020: 'Not good'}
+console.log(years[1999])
+console.log(typeof years[1999])
