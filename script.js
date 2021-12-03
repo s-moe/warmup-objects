ab1a0d4137f34dc80d194b0ce06a54494d26c235
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-12-03 19:14:25 +0000

    restaurant question

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ console.log(person["firstName"])
 //all keys are converted to strings
 const years = {1999: 'Good', 2020: 'Not good'}
 console.log(years[1999])
+console.log(years["2020"])
 console.log(typeof years[1999])
+
+let birthYear = 2020
+
+years[birthYear]
+
+console.log(years[birthYear])
+
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = `${restaurant.address} in ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`
+
+console.log(fullAddress)
